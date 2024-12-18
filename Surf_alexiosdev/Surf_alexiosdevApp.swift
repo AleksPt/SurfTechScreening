d96fcd8833eae05cde7126c87d272c64cf8ea5ea
Alex Petrenko <alex.pt@ya.ru>
2024-12-19 00:52:31 +0300

    Initial Commit

---
+++ Surf_alexiosdev/Surf_alexiosdevApp.swift
@@ -0,0 +1,17 @@
+//
+//  Surf_alexiosdevApp.swift
+//  Surf_alexiosdev
+//
+//  Created by Алексей on 19.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Surf_alexiosdevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
